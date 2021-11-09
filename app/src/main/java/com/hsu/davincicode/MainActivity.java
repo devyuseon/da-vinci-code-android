796b1d4a1733ed7cbcce624f75af467d4d5561b1
임유선 <1991167@hansung.ac.kr>
2021-11-09 23:38:53 +0900

    Feat: 뷰바인딩 추가, 인터넷권한 추가

--- app/src/main/java/com/hsu/davincicode/MainActivity.java
+++ app/src/main/java/com/hsu/davincicode/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.hsu.davincicode.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    // network
+    final String ip_addr = "10.0.2.2"; // Emulator PC의 127.0.0.1
+    final int port_no = 30000;
+    public Socket socket;
+    public ObjectInputStream ois;
+    public ObjectOutputStream oos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
     }
 }
