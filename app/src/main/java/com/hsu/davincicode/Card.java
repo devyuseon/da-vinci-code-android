baf593a68c55ff6cb45a8b70b65e3dac5f6aa1ce
임유선 <1991167@hansung.ac.kr>
2021-12-02 02:55:29 +0900

    Feat: 카드리스트 리시브, 내 카드 초기화, 카드리스트 정렬 구현

--- app/src/main/java/com/hsu/davincicode/Card.java
+++ app/src/main/java/com/hsu/davincicode/Card.java
@@ -1,7 +1,14 @@
 package com.hsu.davincicode;
 
+import androidx.annotation.NonNull;
+
 import java.io.Serializable;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 class Card implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -9,18 +16,26 @@ class Card implements Serializable {
     private int cardNum; // 0~11
     private String cardColor;
     private Boolean isJocker;
+    private Boolean isOpened;
 
     public Card(String color, int num) {
         this.owner = "RoomId";
         this.cardColor = color;
         this.cardNum = num;
-        this.isJocker = false;
+        if (cardNum == -1) this.isJocker = true;
+        else this.isJocker = false;
+        isOpened = false;
     }
 
-    public Card(String color, Boolean isJocker) {
-        this.owner = "RoomId";
-        this.cardColor = color;
-        this.cardNum = -1;
-        this.isJocker = isJocker;
+    public Card() { // 상대방 카드 초기화할때
+        cardNum = 100; // 모르는 카드
+        cardColor = "n"; // 모르는 카드 색은 n
+        isJocker = false; // 모르는 카드 조커 기본값은 false
+        isOpened = false;
+    }
+
+    @Override
+    public String toString() {
+        return "("+ cardColor + " " + cardNum + ")";
     }
 }
--- app/src/main/java/com/hsu/davincicode/GameActivity.java
+++ app/src/main/java/com/hsu/davincicode/GameActivity.java
@@ -1,9 +1,10 @@
 package com.hsu.davincicode;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -11,10 +12,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.hsu.davincicode.databinding.ActivityGameBinding;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameActivity extends AppCompatActivity {
     private ActivityGameBinding binding;
@@ -28,10 +33,15 @@ public class GameActivity extends AppCompatActivity {
     private String userName;
 
     private ArrayList<String> userList = new ArrayList<>();
+    private ArrayList<Card> myCardList = new ArrayList<>();
+    private int leftCardCount = 24;
+    private Map<String, ArrayList<Card>> userCardList = new HashMap<String, ArrayList<Card>>();
+    private Comparator<Card> sortCard;
 
     private Handler handler; // 스레드에서 UI 작업하기 위한 핸들러
     private Boolean isDoReceiveRunning;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         binding = ActivityGameBinding.inflate(getLayoutInflater());
@@ -62,6 +72,27 @@ public class GameActivity extends AppCompatActivity {
         ChatMsg cm = new ChatMsg(userName, "ROOMUSERLIST", roomId);
         networkUtils.sendChatMsg(cm);
 
+        sortCard = new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                // o2 - o1 리턴하면 내림차순, -1 곱하면 오름차순
+
+                if (o2.getCardNum() - o1.getCardNum() == 0) { // 수가 같을 경우에
+                    return compareByColor(o1.getCardColor(), o2.getCardColor());
+                } else {
+                    return (o2.getCardNum() - o1.getCardNum()) * (-1);
+                }
+
+            }
+
+            public int compareByColor(String o1Color, String o2Color) {
+                if (o1Color.equals(o2Color)) return 0;
+                if (o2Color.equals("w") && o1Color.equals("b")) return -1;
+                else return 1;
+            }
+
+        };
+
         binding.btnReady.setOnClickListener(v -> {
             ChatMsg cm1 = new ChatMsg(userName, "READY", roomId);
             networkUtils.sendChatMsg(cm1);
@@ -71,6 +102,7 @@ public class GameActivity extends AppCompatActivity {
     class ReceiveMsgTask extends AsyncTask<ChatMsg, String, Void> {
 
 
+        @RequiresApi(api = Build.VERSION_CODES.N)
         @Override
         protected Void doInBackground(ChatMsg... strings) {
 
@@ -78,21 +110,27 @@ public class GameActivity extends AppCompatActivity {
                 ChatMsg cm = networkUtils.readChatMsg();
 
                 if (!cm.code.isEmpty()) {
-                    Log.d("FromServer[GameActivity]", String.format("code: %s / userName: %s / data: %s / list: %s / cards: %s", cm.code, cm.UserName, cm.data, cm.list.toString(), cm.cards.toString()));
+                    Log.d("FromServer[GameActivity]", String.format("code: %s / userName: %s / data: %s / list: %s", cm.code, cm.UserName, cm.data, cm.list.toString()));
                     publishProgress(cm);
                 }
             }
             return null;
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.N)
         private void publishProgress(ChatMsg cm) {
             handler.post(() -> {
+                int playerCount = cm.list.size() - 1; // 나를 제외한 플레이어 수
+
                 if (cm.code.matches("ROOMUSERLIST") && cm.UserName.equals(userName)) {
-                    int playerCount = cm.list.size() - 1; // 나를 제외한 플레이어 수
+
+                    /* 유저 프로필 이미지뷰, 이름 텍스트뷰 설정 */
+                    userList = cm.list;
+                    userList.remove(userName);
                     int i = 0;
                     TextView[] tvUserNames = {binding.tvPlayer1Name, binding.tvPlayer2Name, binding.tvPlayer3Name};
                     ImageView[] ivUserNames = {binding.ivPlayer1, binding.ivPlayer2, binding.ivPlayer3};
-                    for (String name : cm.list) {
+                    for (String name : userList) {
                         if(!name.equals(userName) && i < playerCount) {
                             tvUserNames[i].setText(name);
                             tvUserNames[i].setVisibility(View.VISIBLE);
@@ -100,9 +138,28 @@ public class GameActivity extends AppCompatActivity {
                             i++;
                         }
                     }
+
                 }
                 if (cm.code.matches("READY")) {
                     binding.btnReady.setVisibility(View.GONE);
+
+                    /* 카드 정보 초기화 */
+                    for (String cardInfo : cm.list) {
+                        myCardList.add(new Card(cardInfo.substring(0,1), Integer.parseInt(cardInfo.substring(1))));
+                    }
+
+                    /* 나 제외 다른 유저 카드 리스트 초기화 */
+                    for (String user: userList) {
+                        ArrayList<Card> cardList = new ArrayList<>();
+                        for (int i = 0; i < myCardList.size(); i++) {
+                            cardList.add(new Card());
+                        }
+                        userCardList.put(user, cardList); // userCardList: <이름, 카드리스트>
+                    }
+
+                    Collections.sort(myCardList, sortCard);
+                    Log.d("CardList[내꺼]", myCardList.toString());
+                    Log.d("CardList[다른사람]", userCardList.toString());
                 }
             });
         }
