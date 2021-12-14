a2480cf66f8c37d79bf9e9de028f563ba6014397
임유선 <1991167@hansung.ac.kr>
2021-12-15 05:04:03 +0900

    Feat: Fail시에 무조건 turn

--- app/src/main/java/com/hsu/davincicode/GameActivity.java
+++ app/src/main/java/com/hsu/davincicode/GameActivity.java
@@ -478,8 +478,7 @@ public class GameActivity extends AppCompatActivity {
 
         final Handler handler = new Handler();
         handler.postDelayed(() -> {
-            if (leftCardsCount > 0)
-                sendMsgToServer(new ChatMsg(userName, "TURN", roomId));
+            sendMsgToServer(new ChatMsg(userName, "TURN", roomId));
         }, 2000);
 
     }
